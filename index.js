a810683f84ac330a9db852d85ac380aba7114f4a
romantrickey-ops <roman.trickey@students.msswarriors.org>
2026-04-23 14:47:18 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 let quote = "...ask not what your country can do for you — ask what you can do for your country.";
-var quote = quoteLength;
+var quote = quote.Length;
+var quote = quote.twentySecondLetter
+var output = quote.Length
 //Add code below
