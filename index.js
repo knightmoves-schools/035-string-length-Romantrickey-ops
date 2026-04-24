2078e30b9893c35c4fca338dc4179936bdd41780
romantrickey-ops <roman.trickey@students.msswarriors.org>
2026-04-24 14:13:46 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 let quote = "...ask not what your country can do for you — ask what you can do for your country.";
-var quote = quoteLength;
-var quote = twentySecondLetter;
-var output = quoteLength;
-var output = twentySecondLetter ([])
+let quoteLength= quote.length;
+let twentySecondLetter = quote[21];
+let output= `this is my string with ${quoteLength} and also [${twentySecondLetter}]`;
+
 document.getElementById("result").innerHTML = output;
 //Add code below
